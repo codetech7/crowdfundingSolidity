88c60d33df27b20836a6774507f996be281df0c8
codetech7 <codetechseven@gmail.com>
2022-06-24 14:00:16 +0100

    Added button spinner and error handling to form.

---
+++ crowdfunding/pages/component/new-charity-project.js
@@ -0,0 +1,61 @@
+import {Form, Input, Button, Message} from 'semantic-ui-react';
+import React, {Component} from 'react';
+// import "semantic-ui-css/semantic.min.css";
+import Layout from '../../component/Layout';
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
+
+class SignUp extends Component{
+    state = {
+        minimumContribution : "",
+        errorMessage: "",
+        loading: false
+    }
+
+    submittal = async (event) => {
+        event.preventDefault();
+        this.setState({errorMessage:""});
+        this.setState({loading:true});
+
+        try {
+            const accounts = await web3.eth.getAccounts();
+        
+            await factory.methods
+            .createCampaign(this.state.minimumContribution)
+            .send({from:accounts[0]});
+        } catch (error) {
+            this.setState({errorMessage: error.message});
+        }
+
+       this.setState({loading:false});
+
+     
+   }
+
+   render(){
+    return (
+    <Layout>
+        <Form onSubmit={this.submittal} error = {!!this.state.errorMessage}>
+            <Form.Field>
+                <Input 
+                label = "wei"
+                labelPosition='right'
+                value = {this.state.minimumContribution}
+
+                onChange={(event)=>{
+                    this.setState({minimumContribution: event.target.value});
+                }}
+                />
+                
+            </Form.Field>
+
+            <Message error content={this.state.errorMessage}/>
+
+            <Button primary loading={this.state.loading} content = "Create"/>
+        </Form>
+    </Layout>
+   )
+}
+}
+
+export default SignUp;
