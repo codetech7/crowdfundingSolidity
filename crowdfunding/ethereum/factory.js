75ce194914c45bd4a414d0b487284e34f276de6d
codetech7 <codetechseven@gmail.com>
2022-06-25 11:54:37 +0100

    Modified factory.js address to new factory address

--- crowdfunding/ethereum/factory.js
+++ crowdfunding/ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CrowdfundingLauncher.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x147459AAdA35e5cc34Fd103c6A2D895071d93Bb6'
+  '0x07a7Faf2C6E1B741c282d2bBE03B267BaA98e69f'
 );
 
 export default instance;
