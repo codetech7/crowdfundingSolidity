c3922d29e051766a10ae4b2793a986cfc9599ef7
codetech7 <codetechseven@gmail.com>
2022-07-02 01:12:28 +0100

    troubleshooting production

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const next = require('next');
 const routes = require('./routes');
-const app = next({dev: process.env.NODE_ENV !== 'production'});
+const app = next({dev: process.env.NODE_ENV == 'production'});
 const handler = routes.getRequestHandler(app);
 
 const {createServer} = require('http');
