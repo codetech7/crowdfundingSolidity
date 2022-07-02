fba2dc3528070beea8076193feea2787ec9fdeaa
codetech7 <codetechseven@gmail.com>
2022-07-02 19:20:32 +0100

    Deployed successfully, working on styling

--- component/Layout.js
+++ component/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import {Container} from "semantic-ui-react";
+import Head from 'next/head';
 
 
 
@@ -8,6 +9,7 @@ import {Container} from "semantic-ui-react";
 const Layout = (props) => {
     return(
   <Container>
+    <Head><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"/></Head>
     <Header  />
         {props.children}
   </Container>
