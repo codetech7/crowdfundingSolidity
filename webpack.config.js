0edf66492680371fa631be5fa6553e9452ad56e3
codetech7 <codetechseven@gmail.com>
2022-07-02 18:13:42 +0100

    Made changes to webpack config

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+//defines the entry and output points for our application
+const path = require('path');
+const SRC_DIR = path.join(__dirname, '/react-client/src');
+const DIST_DIR = path.join(__dirname, '/react-client/dist');
+const webpack = require('webpack');
+module.exports = {
+    entry: `${SRC_DIR}/index.jsx`,
+    output: {
+        path: DIST_DIR,
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json', '.css'],
+    },
+   
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.png$/,
+                loader: 'url-loader?limit=100000&minetype=image/png'
+            },
+            {
+                test: /\.jpg/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.jsx?/,
+                include: SRC_DIR,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                },
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        })
+    ]
+};
